da783695e3beb1f5bf7011fb522e540c7b3434e2
Alexander Tsang <ahytsang@gmail.com>
2025-04-30 11:41:13 -0600

    loginForm: add form validation.

--- apps/web-app/app/login/loginForm.tsx
+++ apps/web-app/app/login/loginForm.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useContext, useState } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+
 import Alert from '@mui/material/Alert'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -9,15 +11,31 @@ import TextField from '@mui/material/TextField'
 
 import { AuthContext } from '@/components/authContext'
 
+interface IFormInputs {
+  username: string
+  password: string
+}
+
 export default function LoginForm() {
   const authContext = useContext(AuthContext)!
 
-  let [username, setUsername] = useState<string>('')
-  let [password, setPassword] = useState<string>('')
+  const {
+    handleSubmit,
+    control: formControl,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
+
   let [errorMessage, setErrorMessage] = useState<string>('')
 
-  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+  const onSubmit: SubmitHandler<IFormInputs> = async function handleLogin(
+    data: IFormInputs
+  ) {
+    const { username, password } = data
     setErrorMessage('')
     try {
       await authContext!.passwordLogin(username, password)
@@ -32,30 +50,50 @@ export default function LoginForm() {
       {authContext.isLoggedIn() && (
         <div>Logged in as {authContext!.state.user!.name}</div>
       )}
-      <Box component="form" onSubmit={handleLogin}>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)}>
         {!authContext.isLoggedIn() && (
           <Stack spacing={2} sx={{ alignItems: 'center' }}>
-            <div>
-              <TextField
-                id="usernameInput"
-                type="text"
-                label="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              ></TextField>
-            </div>
-            <div>
-              <TextField
-                id="passwordInput"
-                type="password"
-                label="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              ></TextField>
-            </div>
-            <div>
-              <Button type="submit" variant="contained">
-                Login
-              </Button>
-            </div>
+            <Controller
+              name="username"
+              control={formControl}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  id="usernameInput"
+                  type="text"
+                  label="Username"
+                  {...field}
+                  error={!!errors.username}
+                  helperText={
+                    errors.username?.type === 'required'
+                      ? 'Username is required'
+                      : ''
+                  }
+                ></TextField>
+              )}
+            />
+            <Controller
+              name="password"
+              control={formControl}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  id="passwordInput"
+                  type="password"
+                  label="Password"
+                  {...field}
+                  error={!!errors.password}
+                  helperText={
+                    errors.password?.type === 'required'
+                      ? 'Password is required'
+                      : ''
+                  }
+                ></TextField>
+              )}
+            />
+            <Button type="submit" variant="contained">
+              Login
+            </Button>
             <div>
               {errorMessage.length > 0 && (
                 <Alert severity="error">{errorMessage}</Alert>
